807456589197c7a91beb038908ca674fada4b096
RamiroPerez <ramiperez1997@hotmail.com>
2022-07-15 16:58:30 -0300

    adding nav-show

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 document.getElementById("btn-main-search").addEventListener("click",function(){
     alert("El boton esta funcionando. Estos son mis primeros pasos en javascript. Hola mundooo");
 });
+
+document.querySelector("#nav-menu").addEventListener("click", () => {
+    console.log("esto funciona");
+    document.querySelector("#nav-menu").classList.toggle("disguise");
+  });
+
+
+
+
+
+
